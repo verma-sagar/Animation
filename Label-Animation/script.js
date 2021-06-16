f1411e21c831806ada018124badf40e0911e85cc
Saga Verma <44877266+verma-sagar@users.noreply.github.com>
2021-06-17 00:21:41 +0530

    Add files via upload

---
+++ Label-Animation/script.js
@@ -0,0 +1,9 @@
+$('input').on('focusin', function() {
+    $(this).parent().find('label').addClass('active');
+  });
+  
+  $('input').on('focusout', function() {
+    if (!this.value) {
+      $(this).parent().find('label').removeClass('active');
+    }
+  });
